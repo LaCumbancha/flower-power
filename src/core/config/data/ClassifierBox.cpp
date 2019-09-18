11b38a2f712f07bf0a5e01e14324e534a2424045
Darius Isamel Maitia Petros <dariusmaitia@gmail.com>
2019-09-18 13:39:08 -0300

    Classifier Box

---
+++ src/core/config/data/ClassifierBox.cpp
@@ -0,0 +1,46 @@
+//
+// Created by darius on 18/9/19.
+//
+
+#include "ClassifierBox.h"
+
+std::string ClassifierBox::serialize() {
+    std::string body;
+    std::string head;
+
+    body = std::to_string(flowerType) + "|";
+    for (auto flower : _flowers) {
+        body += std::to_string(flower.producerId)
+                + "|"
+                + flower.producerName
+                + "|";
+    }
+    head = "|" + std::to_string(body.length()) + "|";
+
+    return head + body;
+}
+
+ClassifierBox ClassifierBox::deserialize(const std::string& serializedCB) {
+    std::vector<std::string> values;
+    std::string value;
+
+    for (auto character : serializedCB) {
+        if (character != '|') {
+            value += character;
+        } else {
+            if (!value.empty()) {
+                values.push_back(value);
+                value = "";
+            }
+        }
+    }
+
+    auto flowerType = (FlowerType)std::stoi(values[1]);
+
+    std::vector<Flower> flowers;
+    for(auto iter = values.begin() + 2; iter < values.end(); iter += 2) {
+        Flower flower = Flower(std::stoi(*iter), *(iter+1));
+        flowers.push_back(flower);
+    }
+    return ClassifierBox(flowerType, flowers);
+}
---
+++ src/core/config/data/ClassifierBox.h
@@ -0,0 +1,31 @@
+//
+// Created by darius on 18/9/19.
+//
+
+#ifndef FLOWER_POWER_CLASSIFIERBOX_H
+#define FLOWER_POWER_CLASSIFIERBOX_H
+
+#include <utility>
+#include <vector>
+#include "Flower.h"
+
+/**
+ * ClassifierBox
+ * Box of 100 flowers of a single kind coming from
+ * different producers
+ */
+class ClassifierBox {
+private:
+    FlowerType flowerType;
+    std::vector<Flower> _flowers;
+
+public:
+    ClassifierBox(FlowerType  flowerType, std::vector<Flower> flowers) : flowerType(flowerType), _flowers(std::move(flowers)) {}
+
+    std::string serialize();
+
+    static ClassifierBox deserialize(const std::string& serializedCB);
+};
+
+
+#endif //FLOWER_POWER_CLASSIFIERBOX_H
